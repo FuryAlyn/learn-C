c90c241c49858d924700aa3d446212424204a277
FuryAlyn <105789697+FuryAlyn@users.noreply.github.com>
2022-05-21 21:48:28 +0800

    Add files via upload

---
+++ 初识循环语句/初识循环语句/初始循环语句.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+int main()
+{
+	int line = 0;
+	while (line<2000)
+	{
+		printf("Ð´nmd´úÂë:%d\n",line);
+		line++;
+	}
+	if (line = 2000)
+		printf("·ÅÆú\n");
+
+	return 0;
+}
